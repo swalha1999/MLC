29a3140ee1b02fa6b5fc4db60ab90ec2f3611c85
swalha1999 <mohamd514.m@gmail.com>
2023-06-18 03:09:36 +0300

    rewrote the whole thing

--- main.cpp
+++ main.cpp
@@ -1,33 +1,50 @@
+#define DEBUG 1
+
 #include <iostream>
 #include "mlc.hpp"
 
-#ifndef number
-#define number 0
-#endif
 
-#define PRINT_M(m) print(m, #m)
+#define PRINT_M(m) std::cout << #m << " := "<< std::endl; (m).print();std::cout << std::endl;
 #define PRINT_I(i) std::cout << #i << " = " << i << std::endl
+#define DELETE(m) delete m; std::cout << #m << " and it was deleted" << std::endl
 #define number 0
 
-void print(const Matrix& m, std::string name){
-    std::cout << name << " := "<< std::endl;
-    m.print();
-    std::cout << std::endl;
-}
 
 int main(){
 
-    Matrix m1(10, 10);
+    Matrix<double>* m1 = new Matrix<double>(10, 10);
 
-    for (double i = 0; i < m1.rows(); i++){
-        for (double j = 0; j < m1.cols(); j++){
-            m1(i, j) += i + j/10;
+    for (double i = 0; i < m1->rows(); i++){
+        for (double j = 0; j < m1->cols(); j++){
+            (*m1)(i, j) += i + j/10;
         }
     }   
 
-    PRINT_M(m1);
-    auto m2 = m1.slice(0, 3, 0, 3);
+    PRINT_M(*m1);
+    m1->Identity();
+
+    
+
+    PRINT_M(*m1);
+    auto m2 = m1->slice(0, 0, 3, 3);
+
+    // DELETE(m1);
+    
+
     PRINT_M(m2);
-    PRINT_M(m1.slice(0, 3, 0, 3));
+
+    std::shared_ptr<double> data = m2.data();
+    
+    PRINT_M(m2.slice(1, 1, 2, 2));
+
+    
+    
+    // try{
+    //     std::cout << "d = " << d[0] << std::endl;
+    // }
+    // catch(std::exception& e){
+    //     std::cout << e.what() << std::endl;
+    // }
+    
 
 }
--- mlc.hpp
+++ mlc.hpp
@@ -1,218 +1,123 @@
 #pragma once
 
 #include <iostream>
+#include <memory>
 
 typedef unsigned long size_m;
 
+template <typename T = double>
 class Matrix {
 private:
     size_m _rows;
     size_m _cols;
+    size_m _start_row;
+    size_m _start_col;
     size_m _skip;
     bool _is_slice;
-    double* _data = nullptr;
+    std::shared_ptr<T> _data = nullptr;
     
 public:
-
+    
     Matrix(){
         _rows = 0;
         _cols = 0;
+        _start_row = 0;
+        _start_col = 0;
         _skip = 0;
         _is_slice = false;
         _data = nullptr;
     }
 
-    Matrix(int rows, int cols){
+    Matrix(size_m rows, size_m cols){
         _rows = rows;
         _cols = cols;
         _skip = cols;
+        _start_row = 0;
+        _start_col = 0;
         _is_slice = false;
-        _data = new double[rows*cols];
+        _data = std::shared_ptr<T>(new T[rows*cols]);
     }
 
-    Matrix(const Matrix& m){
+    Matrix(const Matrix<T>& m){
         _rows = m._rows;
         _cols = m._cols;
+        _start_row = m._start_row;
+        _start_col = m._start_col;
         _skip = m._skip;
         _is_slice = m._is_slice;
         _data = m._data;
     }
 
     ~Matrix(){
-        if (_data != nullptr && !_is_slice) delete[] _data;
-    }
-
-    Matrix& operator=(const Matrix& m){
-        if (this != &m){
-            
-            if (_data != nullptr && !_is_slice){
-                delete[] _data;
-            }
-            
-            _rows = m._rows;
-            _cols = m._cols;
-            _skip = m._skip;
-            _data = m._data;
-            _is_slice = m._is_slice;
-
-        }
-
-        return *this;
-    }
-    
-    Matrix& operator+(const Matrix& m) const {
-        if (_rows != m._rows || _cols != m._cols){
-            throw std::invalid_argument("Matrix dimensions must agree");
-        }
-        Matrix* result = new Matrix(_rows, _cols);
-        for (size_m i = 0; i < _rows; ++i){
-            for (size_m j = 0; j < _cols; ++j){
-                (*result)(i,j) = (*this)(i,j) + m(i,j);
-            }
-        }
-        return *result;
-    }
-
-    Matrix& operator-(const Matrix& m) const {
-        if (_rows != m._rows || _cols != m._cols){
-            throw std::invalid_argument("Matrix dimensions must agree");
-        }
-        Matrix* result = new Matrix(_rows, _cols);
-        for (size_m i = 0; i < _rows; ++i){
-            for (size_m j = 0; j < _cols; ++j){
-                (*result)(i,j) = (*this)(i,j) - m(i,j);
-            }
-        }
-        return *result;
-    }
-    
-    Matrix& operator*(const Matrix& m) const{
-        Matrix* result = new Matrix(_rows, m._cols);
-        for (size_m i = 0; i < _rows; i++){
-            for (size_m j = 0; j < m._cols; j++){
-                double sum = 0;
-                for (size_m k = 0; k < _cols; k++){
-                    sum += _data[i*_skip + k] * m._data[k*m._skip + j];
-                }
-                result->_data[i* result->_skip + j] = sum;
-            }
-        }
-        return *result;
-    }
-
-    Matrix& operator*(double d) const {
-        Matrix* result = new Matrix(_rows, _cols);
-        for (size_m row = 0; row < _rows; row++){
-            for (size_m col = 0; col < _cols; col++){
-                (*result)(row,col) = (*this)(row,col) * d;
-            }
-        }
-        return *result;
-    }
-    
-    Matrix& operator/(double d) const {
-        Matrix* result = new Matrix(_rows, _cols);
-        for (size_m row = 0; row < _rows; row++){
-            for (size_m col = 0; col < _cols; col++){
-                (*result)(row,col) = (*this)(row,col) / d;
-            }
-        }
-        return *result;
-    }
-
-    Matrix& operator+=(const Matrix& m){
-        for (size_m i = 0; i < _rows; i++){
-            for (size_m j = 0; j < _cols; j++){
-                _data[i*_skip + j] += m._data[i*_skip + j];
-            }
-        }
-        return *this;
-    }
-
-    Matrix& operator-=(const Matrix& m){
-       for (size_m i = 0; i < _rows; i++){
-            for (size_m j = 0; j < _cols; j++){
-                _data[i*_skip + j] -= m._data[i*_skip + j];
-            }
-        }
-        return *this;
-    }
-
-    Matrix& operator*=(const Matrix& m){
-        Matrix result(_rows, m._cols);
         
-        for (size_m i = 0; i < _rows; i++){
-            for (size_m j = 0; j < m._cols; j++){
-                double sum = 0;
-                for (size_m k = 0; k < _cols; k++){
-                    sum += _data[i*_skip + k] * m._data[k*m._skip + j];
-                }
-                result._data[i*result._skip + j] = sum;
-            }
-        }
-
-        for (size_m i = 0; i < _rows; i++){
-            for (size_m j = 0; j < m._cols; j++){
-                (*this)(i,j) = result(i, j);
-            }
-        }
+        _data = nullptr;
         
-        return *this;
+        #ifdef DEBUG
+        std::cout << "Matrix destructor called on " ;
+        #endif
+
     }
 
-    double& operator()(size_m i, size_m j) const{
-        if (i >= _rows || j >= _cols){
-            throw std::out_of_range("Index out of range");
-        }
-        return _data[i*_skip + j];
-    }
-
-    int rows() const {
+    const size_m rows() const {
         return _rows;
     }
 
-    int cols() const {
+    const size_m cols() const {
         return _cols;
     }
-    
-    int skip() const {
-        return _skip;
-    }
 
-    double* data() const {
+    std::shared_ptr<T> data() const {
         return _data;
     }
+    
+    T& operator()(size_m row, size_m col) const {
+        size_m row_idx = row + _start_row;
+        size_m col_idx = col + _start_col;
+        return _data.get()[row_idx*_skip + col_idx];
+    }
 
-    void print() const{
+    T& operator[](size_m idx) const {
+        return _data.get()[ _start_row*_skip + _start_col + idx];
+    } 
+
+
+    
+    Matrix<T>& slice(size_m start_row, size_m start_col, size_m end_row, size_m end_col){
+        Matrix<T>* m = new Matrix<T>();
+        m->_rows = end_row - start_row;
+        m->_cols = end_col - start_col;
+        m->_start_row = this->_start_row + start_row;
+        m->_start_col = this->_start_col + start_col;
+        m->_skip = this->_skip;
+        m->_is_slice = true;
+        m->_data = this->_data;
+        return *m;
+    }
+
+    void Identity(){
         for (size_m i = 0; i < _rows; i++){
-            std::cout << "      | ";
             for (size_m j = 0; j < _cols; j++){
-                std::cout << std::to_string(_data[i*_skip + j]) << " ";
+                if (i == j){
+                    (*this)(i, j) = 1;
+                }
+                else{
+                    (*this)(i, j) = 0;
+                }
             }
-            std::cout <<"|" << std::endl;
         }
     }
 
-    Matrix& slice(size_m row_start, size_m row_end, size_m col_start, size_m col_end) const{
-        Matrix* result = new Matrix();
-
-        result->_rows = row_end - row_start;
-        result->_cols = col_end - col_start;
-        result->_skip = _skip;
-        result->_is_slice = true;
-        result->_data = _data + (row_start * _skip) + col_start;
-        
-        return *result;
-    }
-
-    Matrix& copy() const{
-        Matrix* result = new Matrix(_rows, _cols);
+    void print() const {
         for (size_m i = 0; i < _rows; i++){
+            std::cout << "  | ";
             for (size_m j = 0; j < _cols; j++){
-                (*result)(i,j) = (*this)(i,j);
+                std::cout << (*this)(i, j) << " ";
             }
+            std::cout << "|";
+            std::cout << std::endl;
         }
-        return *result;
     }
+
+
 
 };
